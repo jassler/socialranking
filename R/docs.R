dc447d9cf3cf7ce674e2010c4b637ae345c6c00b
jassler <erifetim@gmail.com>
2022-03-28 22:34:44 +0200

    Updated functions list

--- R/docs.R
+++ R/docs.R
@@ -7,27 +7,23 @@
 #' is a convenient function to generate a [`newPowerRelation()`] function call
 #' for all possible coalitions.
 #'
-#' For every function that calculates the score of a given element in
-#' a power relation, there is a `ranking`-equivalent function that
-#' displays the ranking between elements. These include:
+#' The functions used to analyze power relations can be grouped into comparison functions,
+#' score functions and ranking solutions. Ranking solutions produce a `SocialRankingSolution` object.
 #'
-#' |      *Score Function*      |      *Ranking Function*      |
-#' | -------------------------- | ---------------------------- |
-#' | [`ordinalBanzhafScores()`] | [`ordinalBanzhafRanking()`]  |
-#' | [`copelandScores()`]       | [`copelandRanking()`]        |
-#' | [`kramerSimpsonScores()`]  | [`kramerSimpsonRanking()`]   |
-#' | [`lexcelScores()`]         | [`lexcelRanking()`]          |
-#' |                            | [`dualLexcelRanking()`]      |
-#' | [`cumulativeScores()`]     | [`cumulativelyDominates()`]* |
-#' |                            | [`dominates()`]*             |
-#' |                            | [`cpMajorityComparison()`]*  |
-#'
-#' *These functions only compare two elements and doen't rank them.
+#' | Comparison Functions                                      | Score Functions                               | Ranking Solutions                               |
+#' |-----------------------------------------------------------|-----------------------------------------------|-------------------------------------------------|
+#' | `dominates()`                                             |                                               |                                                 |
+#' | `cumulativelyDominates()`                                 | `cumulativeScores()`                          |                                                 |
+#' | `cpMajorityComparison()`<br>`cpMajorityComparisonScore()` | `copelandScores()`<br>`kramerSimpsonScores()` | `copelandRanking()`<br>`kramerSimpsonRanking()` |
+#' |                                                           | `lexcelScores()`                              | `lexcelRanking()`<br>`dualLexcelRanking()`      |
+#' |                                                           | `ordinalBanzhafScores()`                      | `ordinalBanzhafRanking()`                       |
 #'
 #' [`powerRelationMatrix()`] uses [`relations::relation()`] to create
 #' an incidence matrix between all competing coalitions. The incidence
 #' matrix can be displayed with [`relations::relation_incidence()`].
 #'
+#' Use `browseVignettes("socialranking")` for more information.
+#' 
 #' @importFrom Rdpack reprompt
 #' @importFrom relations as.relation
 #' @importFrom mathjaxr preview_rd
