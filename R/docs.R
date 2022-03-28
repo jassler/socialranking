d71f6675be8356e1290f3f8a9e03f6891aa2f1b8
Felix Fritz <felix.fritz@hs-kempten.de>
2022-03-28 22:52:10 +0200

    Updated socialranking documentation

--- R/docs.R
+++ R/docs.R
@@ -10,20 +10,24 @@
 #' The functions used to analyze power relations can be grouped into comparison functions,
 #' score functions and ranking solutions. Ranking solutions produce a `SocialRankingSolution` object.
 #'
-#' | Comparison Functions                                      | Score Functions                               | Ranking Solutions                               |
-#' |-----------------------------------------------------------|-----------------------------------------------|-------------------------------------------------|
-#' | `dominates()`                                             |                                               |                                                 |
-#' | `cumulativelyDominates()`                                 | `cumulativeScores()`                          |                                                 |
-#' | `cpMajorityComparison()`<br>`cpMajorityComparisonScore()` | `copelandScores()`<br>`kramerSimpsonScores()` | `copelandRanking()`<br>`kramerSimpsonRanking()` |
-#' |                                                           | `lexcelScores()`                              | `lexcelRanking()`<br>`dualLexcelRanking()`      |
-#' |                                                           | `ordinalBanzhafScores()`                      | `ordinalBanzhafRanking()`                       |
+#' | Comparison Functions         | Score Functions            | Ranking Solutions           |
+#' |----------------------------  |--------------------------  |-----------------------------|
+#' | [`dominates()`]              |                            |                             |
+#' | [`cumulativelyDominates()`]  | [`cumulativeScores()`]     |                             |
+#' | [`cpMajorityComparison()`]^1 | [`copelandScores()`]       | [`copelandRanking()`]       |
+#' |                              | [`kramerSimpsonScores()`]  | [`kramerSimpsonRanking()`]  |
+#' |                              | [`lexcelScores()`]         | [`lexcelRanking()`]         |
+#' |                              |                            | [`dualLexcelRanking()`]     |
+#' |                              | [`ordinalBanzhafScores()`] | [`ordinalBanzhafRanking()`] |
+#'
+#' ^1 [`cpMajorityComparisonScore()`] is a faster alternative to [`cpMajorityComparison()`], but it produces less data.
 #'
 #' [`powerRelationMatrix()`] uses [`relations::relation()`] to create
 #' an incidence matrix between all competing coalitions. The incidence
 #' matrix can be displayed with [`relations::relation_incidence()`].
 #'
 #' Use `browseVignettes("socialranking")` for more information.
-#' 
+#'
 #' @importFrom Rdpack reprompt
 #' @importFrom relations as.relation
 #' @importFrom mathjaxr preview_rd
