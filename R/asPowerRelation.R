5d460657c17eed49d5da799ed94087b7bee0f208
jassler <123160239+DauphineWeb@users.noreply.github.com>
2023-02-22 02:46:49 +0100

    Added documentation for as.PowerRelation

--- R/asPowerRelation.R
+++ R/asPowerRelation.R
@@ -13,9 +13,36 @@ as.PowerRelation <- function(x, ...) {
 
 #' @section Using a character string:
 #'
-#' The same way a power relation \eqn{\succeq}{>= }
+#' The same way a power relation \eqn{\succeq}{>=} may be represented in literature (or printed by an [`PowerRelation`] object),
+#' a simple string containing letters, numbers, `>` or `~` can be used to input a new power relation.
+#'
+#' Every special character is ignored, with the exception of \eqn{\succeq}{\\succeq} (`"\u227B"`) and \eqn{\sim}{\\sim} (`"\u223C"`).
+#'
+#' Every letter or number is assumed to be an individual element.
+#' `"abc > ac"` therefore would represent two coalitions, the first one of size 3 with the elements `a`, `b`, and `c`.
+#' This method does not allow for elements to be entered that are supposed to be multiple characters long.
+#'
+#' An empty coalitions can be simply left blank (i.e., `"abc > ~ ac"`),
+#' though it is often clearer if curly braces are used to indicate such (i.e., `"abc > {} ~ ac"`).
 #'
 #' @rdname as.PowerRelation
+#'
+#' @examples
+#' # Using character strings
+#' as.PowerRelation("abc > ab > ({} ~ c) > (a ~ b ~ ac) > bc")
+#' # abc > ab > ({} ~ c) > (a ~ b ~ ac) > bc
+#'
+#' # using createPowerset(), then shifting coalitions up and down using Alt+Up and Alt+Down
+#' if(interactive()) {
+#'   createPowerset(1:2, result = "copy")
+#' }
+#' as.PowerRelation("
+#'   12
+#'   > 1
+#'   ~ {}
+#'   > 2
+#' ")
+#'
 #' @export
 as.PowerRelation.character <- function(x, ...) {
   x <- gsub('[^0-9a-zA-Z>~\u227B\u223C]', '', x)
@@ -56,11 +83,38 @@ as.PowerRelation.character <- function(x, ...) {
 
 #' @section Using a list:
 #'
-#' Man man man.
+#' Create a [`PowerRelation`] object with an unnested list of coalition [vectors][base::c()].
+#'
+#' By default, a linear order is assumed on the coalitions.
+#' I.e., if it is given `list(c(1,2),1,2)`, these three coalitions are put into their own equivalence class,
+#' producing `12 > 1 > 2`.
+#'
+#' The comparators in-between can be adjusted to indicate
+#' whether the relation between two coalitions is that of strict preference `>` or indifference `~`.
 #'
 #' @rdname as.PowerRelation
+#'
+#' @examples
+#' # Using lists
+#' as.PowerRelation(list(c(1,2), 2, c(), 1))
+#' # 12 > 2 > {} > 1
+#'
+#' as.PowerRelation(list(c(1,2), 2, c(), 1), comparators = c("~", ">", ">"))
+#' # (12 ~ 2) > {} > 1
+#'
+#' # the length of comparators doesn't necessarily matter.
+#' # If comparators are missing, the existing ones are simply repeated...
+#' as.PowerRelation(list(c(1,2), 2, c(), 1), comparators = "~")
+#' # (12 ~ 2 ~ {} ~ 1)
+#'
+#' as.PowerRelation(list(c(1,2), 2, c(), 1), comparators = c("~", ">"))
+#' # (12 ~ 2) > ({} ~ 1)
+#'
+#' # ... or the rest is cut off
+#' as.PowerRelation(list(c(1,2), 2, c(), 1), comparators = c("~", ">", "~", "~", ">"))
+#' # (12 ~ 2) > ({} ~ 1)
 #' @export
-as.PowerRelation.list <- function(x, ..., comparators = c('>')) {
+as.PowerRelation.list <- function(x, ..., comparators = c(">")) {
   eqs <- list()
   eq <- list()
   comparators <- rep(comparators, length.out = length(x) - 1)
